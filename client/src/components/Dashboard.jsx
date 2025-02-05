600b4ed9b84699da804e8f6968f09ee7a6fe9d93
Rebecca Liu <qu6qu6g@gmail.com>
2025-02-04 20:59:11 -0500

    added hamburger menu

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,12 +1,21 @@
-
+import { TbMenu2 } from "react-icons/tb";
 
 const Dashboard = () => {
   return (
     <>
       <div className="flex flex-col h-screen">
-        <div className="w-screen text-center p-4 border-red-500 border-2 h-[33%] flex justify-center items-center">
-          <p className="font-mono font-semibold text-3xl">Welcome username</p>
+        {/* Welcome Text */}
+        <div className="w-screen p-4 border-red-500 border-2 h-[25%] flex justify-center items-center relative">
+          <button className="bg-[#B3B3B3]  p-2 rounded-full hover:scale-110 duration-200 absolute top-3 left-3">
+            <TbMenu2 size="1.5em" />
+          </button>
+          <div>
+            <h1 className="font-mono font-semibold text-3xl pt-4">Welcome username</h1>
+          </div>
         </div>
+
+
+        {/* Balance Section */}
         <div className="h-screen  border-red-500 border-2">
           <div className="bg-[#2B363C] h-[85%] m-6  border-green-500 border-2">
             <div className="flex flex-col h-[100%] ">
@@ -15,6 +24,8 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
+
+
       </div>
     </>
   )
