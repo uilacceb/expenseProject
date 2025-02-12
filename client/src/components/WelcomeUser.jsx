c36ba3ae30f84e94dbb528026a9e8d4a227d5b2f
Rebecca Liu <qu6qu6g@gmail.com>
2025-02-12 00:35:15 -0500

    testing changing screen height in mobile view

--- client/src/components/WelcomeUser.jsx
+++ client/src/components/WelcomeUser.jsx
@@ -2,14 +2,14 @@ import { useContext } from "react";
 import { ExpenseContext } from "../App";
 import Balance from "./Balance";
 import HamburgerMenu from "./HamburgerMenu";
-import Footer from "./Footer";
+
 
 
 const WelcomeUser = () => {
   const { user } = useContext(ExpenseContext)
   return (
     <>
-      <div className="flex flex-col w-full h-screen justify-center items-center relative">
+      <div className="flex flex-col w-full lg:h-screen h-[90vh] justify-center items-center relative">
         <HamburgerMenu />
         <div className="h-[30%] flex items-center justify-center">
           <h1 className="font-mono font-semibold text-3xl pt-4 lg:text-[50px] caret-transparent">{user ? (`Welcome ${user.given_name}!`) : "Welcome Guest!"}</h1>
