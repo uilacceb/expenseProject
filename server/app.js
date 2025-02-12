99ff7e351448b58941e4498e50f1f92eae5c84bc
Rebecca Liu <qu6qu6g@gmail.com>
2025-02-11 22:41:04 -0500

    modified file for deployment

--- server/app.js
+++ server/app.js
@@ -1,31 +1,30 @@
 import express from "express"
 import { connectDB } from "./db.js"
 import dotenv from "dotenv"
-import cors from 'cors';
-import ExpenseRouter from "./routes/expense.route.js";
+import cors from 'cors'
+import ExpenseRouter from "./routes/expense.route.js"
+
 dotenv.config()
 
 const app = express();
+
+// Move CORS and middleware setup to top
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
 app.use(express.json())
+
+// Connect to database
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to expense Application" })
 })
 
-app.listen(8000, () => {
-  connectDB();
-  console.log(`Server is running on port 8000`);
-})
-
-
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-
-app.use(express.json())
 app.use(ExpenseRouter)
 
-export default app;
+// Remove the app.listen() call - Vercel handles this
+
+export default app
